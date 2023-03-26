353d94145be3e9a698a81e4ed634a16e1df514e6
mohit <54810032+mohitgupta-16@users.noreply.github.com>
2023-03-26 18:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+function majorityElement(arr) {
+  let count = 1;
+  let majority = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === majority) {
+      count++;
+    } else {
+      count--;
+      if (count === 0) {
+        majority = arr[i];
+        count = 1;
+      }
+    }
+  }
+
+  // Count the occurrences of majority element
+  count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === majority) {
+      count++;
+    }
+  }
+
+  // Check if majority element appears more than floor(n/2) times
+  if (count > Math.floor(arr.length / 2)) {
+    return majority;
+  } else {
+    return -1;
+  }
+}
